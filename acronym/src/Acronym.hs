963640b803ad8d3f9c37dddaf5234008c4e5ca19
rapidfireworks <rapidfireworks@gmail.com>
2020-08-15 14:52:17 +0900

    Solve

--- acronym/src/Acronym.hs
+++ acronym/src/Acronym.hs
@@ -1,4 +1,22 @@
 module Acronym (abbreviate) where
 
+import qualified Data.Char
+
 abbreviate :: String -> String
-abbreviate xs = error "You need to implement this function."
+abbreviate xs =
+  let
+    abbr "" = ""
+    abbr (_:"") = ""
+    abbr (first:rest@(second:_))
+      | isAbbrSeparator first && Data.Char.isAlpha second
+      = Data.Char.toUpper second:abbr rest
+      | Data.Char.isLower first && Data.Char.isUpper second
+      = second:abbr rest
+      | otherwise
+      = abbr rest
+
+    isAbbrSeparator '-' = True
+    isAbbrSeparator '_' = True
+    isAbbrSeparator letter = Data.Char.isSpace letter
+  in
+    abbr (' ':xs)
