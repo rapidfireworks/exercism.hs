746829a7162e0aa9fc5027356053eac965e35fa9
rapidfireworks <rapidfireworks@gmail.com>
2020-08-16 19:10:37 +0900

    Solve

--- space-age/src/SpaceAge.hs
+++ space-age/src/SpaceAge.hs
@@ -10,4 +10,17 @@ data Planet = Mercury
             | Neptune
 
 ageOn :: Planet -> Float -> Float
-ageOn planet seconds = error "You need to implement this function."
+ageOn planet seconds = seconds / (earthYear * periodRatio planet)
+
+earthYear :: Float
+earthYear = 31557600
+
+periodRatio :: Planet -> Float
+periodRatio Mercury = 0.2408467
+periodRatio Venus = 0.61519726
+periodRatio Earth = 1
+periodRatio Mars = 1.8808158
+periodRatio Jupiter = 11.862615
+periodRatio Saturn = 29.447498
+periodRatio Uranus = 84.016846
+periodRatio Neptune = 164.79132
