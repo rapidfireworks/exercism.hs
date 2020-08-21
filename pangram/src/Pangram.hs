f7645c0b2f957e6efee3c08650fae1327aebaec6
rapidfireworks <rapidfireworks@gmail.com>
2020-08-21 15:57:49 +0900

    Solve

--- pangram/src/Pangram.hs
+++ pangram/src/Pangram.hs
@@ -1,4 +1,12 @@
 module Pangram (isPangram) where
 
+import qualified Data.Char as Char
+import qualified Data.Set as Set
+
 isPangram :: String -> Bool
-isPangram text = error "You need to implement this function."
+isPangram = (26 ==) . length . letterSet
+
+letterSet :: String -> Set.Set Char
+letterSet text
+  = Set.fromList [Char.toLower x | x <- text, isAlphabet x]
+  where isAlphabet x = Char.isAsciiLower x || Char.isAsciiUpper x
