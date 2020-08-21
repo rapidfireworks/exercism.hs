2dade0a5460d523b7e0f8874c70d9393a0fd4e80
rapidfireworks <rapidfireworks@gmail.com>
2020-08-21 14:46:00 +0900

    Solve

--- perfect-numbers/src/PerfectNumbers.hs
+++ perfect-numbers/src/PerfectNumbers.hs
@@ -1,6 +1,29 @@
 module PerfectNumbers (classify, Classification(..)) where
 
+import qualified Data.Foldable as Foldable
+
 data Classification = Deficient | Perfect | Abundant deriving (Eq, Show)
 
 classify :: Int -> Maybe Classification
-classify = error "You need to implement this function."
+classify n
+  | n < 1
+  = Nothing
+classify n
+  | result > n
+  = Just Abundant
+  | result == n
+  = Just Perfect
+  | otherwise
+  = Just Deficient
+  where result = aliquotSum n
+
+aliquotSum :: Int -> Int
+aliquotSum n
+  | divMod n truncSqrt == (truncSqrt,  0)
+  = sumOfDivisors - truncSqrt
+  | otherwise
+  = sumOfDivisors
+  where
+    truncSqrt = (truncate . sqrt . fromIntegral) n
+    divisors = [x | x <- [2..truncSqrt], rem n x == 0]
+    sumOfDivisors = 1 + Foldable.foldl' (\x y -> x + y + div n y) 0 divisors
