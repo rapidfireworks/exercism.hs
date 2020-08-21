58f8a2f6bab7c0cb525ae707ce41ac0314704b78
rapidfireworks <rapidfireworks@gmail.com>
2020-08-21 16:08:20 +0900

    Refactor

--- pangram/src/Pangram.hs
+++ pangram/src/Pangram.hs
@@ -4,9 +4,12 @@ import qualified Data.Char as Char
 import qualified Data.Set as Set
 
 isPangram :: String -> Bool
-isPangram = (26 ==) . length . letterSet
+isPangram = (numberOfAlphabets ==) . length . letterSet
 
 letterSet :: String -> Set.Set Char
 letterSet text
   = Set.fromList [Char.toLower x | x <- text, isAlphabet x]
   where isAlphabet x = Char.isAsciiLower x || Char.isAsciiUpper x
+
+numberOfAlphabets :: Int
+numberOfAlphabets = length ['a'..'z']
