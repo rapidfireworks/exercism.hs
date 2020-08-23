f9817572fe9bbd182f3a1a84596fce2b857973ba
rapidfireworks <rapidfireworks@gmail.com>
2020-08-23 10:33:14 +0900

    Solve

--- strain/src/Strain.hs
+++ strain/src/Strain.hs
@@ -1,7 +1,10 @@
 module Strain (keep, discard) where
 
 discard :: (a -> Bool) -> [a] -> [a]
-discard p xs = error "You need to implement this function."
+discard p xs = keep (not . p) xs
 
 keep :: (a -> Bool) -> [a] -> [a]
-keep p xs = error "You need to implement this function."
+keep _ [] = []
+keep p (x : xs)
+  | p x = x : keep p xs
+  | otherwise = keep p xs
