28147982df677a65ca690916d253684fa454f492
rapidfireworks <rapidfireworks@gmail.com>
2020-08-23 10:23:11 +0900

    Refactor without filtering input

--- pangram/src/Pangram.hs
+++ pangram/src/Pangram.hs
@@ -4,12 +4,7 @@ import qualified Data.Char as Char
 import qualified Data.Set as Set
 
 isPangram :: String -> Bool
-isPangram = (numberOfAlphabets ==) . length . letterSet
-
-letterSet :: String -> Set.Set Char
-letterSet text
-  = Set.fromList [Char.toLower x | x <- text, isAlphabet x]
-  where isAlphabet x = Char.isAsciiLower x || Char.isAsciiUpper x
-
-numberOfAlphabets :: Int
-numberOfAlphabets = length ['a'..'z']
+isPangram text = all inText ['a' .. 'z']
+  where
+    set = Set.fromList [Char.toLower x | x <- text]
+    inText letter = Set.member letter set
