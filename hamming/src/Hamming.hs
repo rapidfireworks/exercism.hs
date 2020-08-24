21c187502051eff700356efbed25e2de0176054c
rapidfireworks <rapidfireworks@gmail.com>
2020-08-24 12:00:34 +0900

    Refactor to be more readable

--- hamming/src/Hamming.hs
+++ hamming/src/Hamming.hs
@@ -1,36 +1,10 @@
 module Hamming (distance) where
 
-import Control.Applicative (liftA2)
-import Data.Foldable (foldl')
-import Flow ((|>))
-
 distance :: String -> String -> Maybe Int
 distance xs ys
-  = maybeZip xs ys
-  |> map diffScore
-  |> takeUntil (Nothing ==)
-  |> foldl' (liftA2 (+)) (Just 0)
-
-maybeZip :: [a] -> [a] -> [(Maybe a, Maybe a)]
-maybeZip [] [] = []
-maybeZip [] ys = [(Nothing, Just y) | y <- ys]
-maybeZip xs [] = [(Just x, Nothing) | x <- xs]
-maybeZip (x:xs) (y:ys) = (Just x, Just y):maybeZip xs ys
-
-diffScore :: Eq a => (Maybe a, Maybe a) -> Maybe Int
-diffScore (Nothing, _) = Nothing
-diffScore (_, Nothing) = Nothing
-diffScore (lhs, rhs)
-  | lhs == rhs
-  = Just 0
-  | otherwise
-  = Just 1
-
-takeUntil :: (a -> Bool) -> [a] -> [a]
-takeUntil _ [] = []
-takeUntil _ [x] = [x]
-takeUntil f (x:xs)
-  | f x
-  = [x]
-  | otherwise
-  = x:takeUntil f xs
+  | length xs /= length ys = Nothing
+  | otherwise = (Just . sum . (map diffScore)) (zip xs ys)
+  where
+    diffScore (lhs, rhs)
+      | lhs == rhs = 0
+      | otherwise = 1
