57fb654fa032c97100741129fb78f849629201b3
rapidfireworks <rapidfireworks@gmail.com>
2020-08-24 19:16:53 +0900

    Refactor with regex

--- bob/src/Bob.hs
+++ bob/src/Bob.hs
@@ -1,22 +1,15 @@
 module Bob (responseFor) where
 
-import Data.Char (isLetter, isSpace, isUpper)
+import Text.Regex.TDFA ((=~))
 
 responseFor :: String -> String
 responseFor xs
-  | silent xs = "Fine. Be that way!"
-  | yelling xs && asking xs = "Calm down, I know what I'm doing!"
-  | asking xs = "Sure."
-  | yelling xs = "Whoa, chill out!"
+  | silent = "Fine. Be that way!"
+  | yelling && asking = "Calm down, I know what I'm doing!"
+  | asking = "Sure."
+  | yelling = "Whoa, chill out!"
   | otherwise = "Whatever."
-
-yelling xs = length letters > 0 && all isUpper letters
   where
-    letters = [x | x <- xs, isLetter x]
-
-asking "" = False
-asking ('?' : xs)
-  | all isSpace xs = True
-asking (_ : xs) = asking xs
-
-silent xs = all isSpace xs
+    silent = xs =~ "\\`[[:space:]]*\\'"
+    yelling = not (xs =~ "[[:lower:]]") && xs =~ "[[:upper:]]"
+    asking = xs =~ "\\?[[:space:]]*\\'"
