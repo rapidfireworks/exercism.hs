f2b42bcc002ca031d58334b90e1c54be8632355a
rapidfireworks <rapidfireworks@gmail.com>
2020-08-24 18:12:01 +0900

    Solve

--- bob/src/Bob.hs
+++ bob/src/Bob.hs
@@ -1,4 +1,22 @@
 module Bob (responseFor) where
 
+import Data.Char (isLetter, isSpace, isUpper)
+
 responseFor :: String -> String
-responseFor xs = error "You need to implement this function."
+responseFor xs
+  | silent xs = "Fine. Be that way!"
+  | yelling xs && asking xs = "Calm down, I know what I'm doing!"
+  | asking xs = "Sure."
+  | yelling xs = "Whoa, chill out!"
+  | otherwise = "Whatever."
+
+yelling xs = length letters > 0 && all isUpper letters
+  where
+    letters = [x | x <- xs, isLetter x]
+
+asking "" = False
+asking ('?' : xs)
+  | all isSpace xs = True
+asking (_ : xs) = asking xs
+
+silent xs = all isSpace xs
