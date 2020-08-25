aabdd4a179195518bef1183d821a206e8413a0b6
rapidfireworks <rapidfireworks@gmail.com>
2020-08-25 16:07:57 +0900

    Refactor

--- collatz-conjecture/src/CollatzConjecture.hs
+++ collatz-conjecture/src/CollatzConjecture.hs
@@ -3,10 +3,9 @@ module CollatzConjecture (collatz) where
 collatz :: Integer -> Maybe Integer
 collatz = collatz 0
   where
-    collatz _ x
-      | x < 1 = Nothing
-    collatz n 1 = Just n
     collatz n x
+      | x < 1 = Nothing
+      | x == 1 = Just n
       | r == 0 = collatz (n + 1) q
       | otherwise = collatz (n + 1) (3 * x + 1)
       where
