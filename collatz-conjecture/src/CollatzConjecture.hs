c96670c22f42f93c097b234bd656f993acd443ff
rapidfireworks <rapidfireworks@gmail.com>
2020-08-25 15:14:33 +0900

    Solve

--- collatz-conjecture/src/CollatzConjecture.hs
+++ collatz-conjecture/src/CollatzConjecture.hs
@@ -1,4 +1,13 @@
 module CollatzConjecture (collatz) where
 
 collatz :: Integer -> Maybe Integer
-collatz = error "You need to implement this function."
+collatz = collatz 0
+  where
+    collatz _ x
+      | x < 1 = Nothing
+    collatz n 1 = Just n
+    collatz n x
+      | r == 0 = collatz (n + 1) q
+      | otherwise = collatz (n + 1) (3 * x + 1)
+      where
+        (q, r) = quotRem x 2
