cc57c1854c059718efedb5fb36f4baf92a3c3d69
rapidfireworks <rapidfireworks@gmail.com>
2020-08-27 11:02:51 +0900

    Solve

--- nucleotide-count/src/DNA.hs
+++ nucleotide-count/src/DNA.hs
@@ -1,8 +1,19 @@
-module DNA (nucleotideCounts, Nucleotide(..)) where
+module DNA (nucleotideCounts, Nucleotide (..)) where
 
+import Control.Monad (foldM)
 import Data.Map (Map)
+import qualified Data.Map as Map (empty, insertWith)
 
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
 
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
-nucleotideCounts xs = error "You need to implement this function."
+nucleotideCounts xs = foldM count Map.empty xs
+  where
+    count acc val = case (fromChar val) of
+      Just nucleotide -> Right (Map.insertWith (+) nucleotide 1 acc)
+      Nothing -> Left xs
+    fromChar 'A' = Just A
+    fromChar 'C' = Just C
+    fromChar 'G' = Just G
+    fromChar 'T' = Just T
+    fromChar _ = Nothing
