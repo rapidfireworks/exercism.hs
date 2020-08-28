798797d9f4e171e3808ff3e6920d3230261f31fd
rapidfireworks <rapidfireworks@gmail.com>
2020-08-28 13:17:16 +0900

    Solve

--- sum-of-multiples/src/SumOfMultiples.hs
+++ sum-of-multiples/src/SumOfMultiples.hs
@@ -1,4 +1,9 @@
 module SumOfMultiples (sumOfMultiples) where
 
 sumOfMultiples :: [Integer] -> Integer -> Integer
-sumOfMultiples factors limit = error "You need to implement this function."
+sumOfMultiples factors limit = sum [x | x <- [1..pred limit], isMultipleOfAny x]
+  where
+    isMultiple dividend divisor
+      | divisor > 0 = rem dividend divisor == 0
+      | otherwise = False
+    isMultipleOfAny x = any (isMultiple x) factors
