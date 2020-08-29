afbe0be6662507ef3647874610ca149a6c62ac9d
rapidfireworks <rapidfireworks@gmail.com>
2020-08-29 10:13:59 +0900

    Refactor with Set

--- sum-of-multiples/src/SumOfMultiples.hs
+++ sum-of-multiples/src/SumOfMultiples.hs
@@ -1,9 +1,17 @@
 module SumOfMultiples (sumOfMultiples) where
 
+import Data.Function ((&))
+import Data.Set (Set)
+import qualified Data.Set as Set (empty, fromList, union)
+
 sumOfMultiples :: [Integer] -> Integer -> Integer
-sumOfMultiples factors limit = sum [x | x <- [1..pred limit], isMultipleOfAny x]
-  where
-    isMultiple dividend divisor
-      | divisor > 0 = rem dividend divisor == 0
-      | otherwise = False
-    isMultipleOfAny x = any (isMultiple x) factors
+sumOfMultiples factors limit =
+  factors
+    & map (multiples (pred limit))
+    & foldl Set.union Set.empty
+    & sum
+
+multiples :: Integer -> Integer -> Set Integer
+multiples limit factor
+  | factor > 0 = Set.fromList [factor, 2 * factor .. limit]
+  | otherwise = Set.empty
