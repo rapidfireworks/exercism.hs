28b94d16aa19f7dd428d88c8e779a27420fbf173
rapidfireworks <rapidfireworks@gmail.com>
2020-08-30 13:18:27 +0900

    Solve

--- grains/src/Grains.hs
+++ grains/src/Grains.hs
@@ -1,7 +1,12 @@
 module Grains (square, total) where
 
+import Data.Ix (inRange)
+import Data.Maybe (fromJust)
+
 square :: Integer -> Maybe Integer
-square n = error "You need to implement this function."
+square n
+  | inRange (1, 64) n = Just (2 ^ pred n)
+  | otherwise = Nothing
 
 total :: Integer
-total = error "You need to implement this function."
+total = sum [fromJust (square x) | x <- [1 .. 64]]
