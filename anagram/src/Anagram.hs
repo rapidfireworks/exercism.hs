609dad3afd519b62f49c156de9244b2fabd89ba7
rapidfireworks <rapidfireworks@gmail.com>
2020-08-30 20:16:43 +0900

    Solve

--- anagram/src/Anagram.hs
+++ anagram/src/Anagram.hs
@@ -1,4 +1,19 @@
 module Anagram (anagramsFor) where
 
+import Data.Char (toLower)
+import Data.Map (Map)
+import qualified Data.Map as Map (fromListWith)
+
 anagramsFor :: String -> [String] -> [String]
-anagramsFor xs xss = error "You need to implement this function."
+anagramsFor xs xss = filter isAnagram xss
+  where
+    (xsLower, xsFreq) = lowerFreq xs
+    isAnagram ys = xsLower /= ysLower && xsFreq == ysFreq
+      where
+        (ysLower, ysFreq) = lowerFreq ys
+
+lowerFreq :: String -> (String, Map Char Int)
+lowerFreq xs = (lower, frequency)
+  where
+    lower = map toLower xs
+    frequency = Map.fromListWith (+) [(x, 1) | x <- lower]
