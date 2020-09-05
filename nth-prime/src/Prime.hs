5dfb6975158f03da2e8cd5985bb379a302c80ba8
rapidfireworks <rapidfireworks@gmail.com>
2020-09-05 17:45:49 +0900

    Solve

--- nth-prime/src/Prime.hs
+++ nth-prime/src/Prime.hs
@@ -1,4 +1,16 @@
 module Prime (nth) where
 
+import Control.Lens (element, (^?))
+
 nth :: Int -> Maybe Integer
-nth n = error "You need to implement this function."
+nth n
+  | n < 1 = Nothing
+  | otherwise = [x | x <- candidate, isPrime x] ^? element (pred n)
+  where
+    candidate = 2 : 3 : (scanl1 (+) (5 : cycle [2, 4]))
+
+isPrime :: Integral a => a -> Bool
+isPrime x = x > 1 && all (notFactorOf x) [2 .. truncSqrt x]
+  where
+    truncSqrt = truncate . (sqrt :: Double -> Double) . fromIntegral
+    notFactorOf dividend divisor = rem dividend divisor /= 0
