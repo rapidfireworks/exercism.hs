a5ff160b9b98456fe37ec5a6faa9778b30fcc50d
rapidfireworks <rapidfireworks@gmail.com>
2020-09-05 17:48:02 +0900

    Solve

--- luhn/src/Luhn.hs
+++ luhn/src/Luhn.hs
@@ -1,4 +1,28 @@
 module Luhn (isValid) where
 
+import Text.Read (readMaybe)
+
 isValid :: String -> Bool
-isValid n = error "You need to implement this function."
+isValid n = case (luhnSum n) of
+  Just x -> rem x 10 == 0
+  Nothing -> False
+
+luhnSum :: String -> Maybe Int
+luhnSum digits = do
+  parsed <- parse digits
+  case parsed of
+    xs@(_ : _ : _) -> return $ sum [clamp x | x <- zipWith (*) (cycle [1, 2]) $ reverse xs]
+    _ -> Nothing
+  where
+    clamp x = if x < 10 then x else x - 9
+
+parse :: String -> Maybe [Int]
+parse text = do
+  digits <- mapM readChar text
+  return [digit | Right digit <- digits]
+
+readChar :: Char -> Maybe (Either Char Int)
+readChar letter = case (letter, readMaybe [letter]) of
+  (' ', _) -> Just (Left ' ')
+  (_, Just digit) -> Just (Right digit)
+  (_, Nothing) -> Nothing
