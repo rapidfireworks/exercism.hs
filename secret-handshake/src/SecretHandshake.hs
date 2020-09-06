5808d632274c46e4d93326bd08aa4634ae4f5f7e
rapidfireworks <rapidfireworks@gmail.com>
2020-09-06 18:01:41 +0900

    Solve

--- secret-handshake/src/SecretHandshake.hs
+++ secret-handshake/src/SecretHandshake.hs
@@ -1,4 +1,19 @@
 module SecretHandshake (handshake) where
 
+import Data.Bits
+
 handshake :: Int -> [String]
-handshake n = error "You need to implement this function."
+handshake n = do
+  position <- positions
+  decode (n .&. (bit position))
+  where
+    positions
+      | n .&. bit 4 /= 0 = [3, 2 .. 0]
+      | otherwise = [0 .. 3]
+
+decode :: Int -> [String]
+decode 1 = ["wink"]
+decode 2 = ["double blink"]
+decode 4 = ["close your eyes"]
+decode 8 = ["jump"]
+decode _ = []
