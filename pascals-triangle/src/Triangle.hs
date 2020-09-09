51e1968e7f116d74aa2a05bc1de650047f0011ab
rapidfireworks <rapidfireworks@gmail.com>
2020-09-09 11:08:41 +0900

    Solve

--- pascals-triangle/src/Triangle.hs
+++ pascals-triangle/src/Triangle.hs
@@ -1,4 +1,12 @@
 module Triangle (rows) where
 
 rows :: Int -> [[Integer]]
-rows x = error "You need to implement this function."
+rows x = triangle x [1]
+  where
+    triangle n row | n > 0 = row : triangle (n - 1) (next row)
+    triangle _ _ = []
+
+next :: [Integer] -> [Integer]
+next row = zipWith (+) period (tail $ cycle period)
+  where
+    period = 0 : row
