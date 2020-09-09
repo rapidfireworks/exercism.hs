929b506790c4f6767dca3d873efc0b2d75fe41cb
rapidfireworks <rapidfireworks@gmail.com>
2020-09-09 11:15:11 +0900

    Refactor with iterate

--- pascals-triangle/src/Triangle.hs
+++ pascals-triangle/src/Triangle.hs
@@ -1,12 +1,6 @@
 module Triangle (rows) where
 
 rows :: Int -> [[Integer]]
-rows x = triangle x [1]
+rows x = take x $ iterate next [1]
   where
-    triangle n row | n > 0 = row : triangle (n - 1) (next row)
-    triangle _ _ = []
-
-next :: [Integer] -> [Integer]
-next row = zipWith (+) period (tail $ cycle period)
-  where
-    period = 0 : row
+    next row = zipWith (+) (0 : row) (row ++ [0])
