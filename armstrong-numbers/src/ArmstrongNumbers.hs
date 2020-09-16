0d5a72d9704a9fdd598045db6ed34ae7b00ad300
rapidfireworks <rapidfireworks@gmail.com>
2020-09-16 14:52:58 +0900

    Solve

--- armstrong-numbers/src/ArmstrongNumbers.hs
+++ armstrong-numbers/src/ArmstrongNumbers.hs
@@ -1,4 +1,18 @@
 module ArmstrongNumbers (armstrong) where
 
 armstrong :: Integral a => a -> Bool
-armstrong = error "You need to implement this function."
+armstrong n = armstrongSum == toInteger n
+  where
+    armstrongSum = sum $ do
+      digit <- show $ toInteger n
+      return (read [digit] ^ digitCount n)
+
+digitCount :: (Integral a) => a -> a
+digitCount n | n < 0 = digitCount $ abs n
+digitCount 0 = 1
+digitCount n = 1 + truncLog n
+  where
+    truncLog = truncate . log10 . fromIntegral
+
+log10 :: Double -> Double
+log10 = logBase 10
