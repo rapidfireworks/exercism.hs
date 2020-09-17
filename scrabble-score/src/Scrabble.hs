9514f75011d29bdc87a981b85efcf21cd81f3050
rapidfireworks <rapidfireworks@gmail.com>
2020-09-17 14:04:01 +0900

    Solve

--- scrabble-score/src/Scrabble.hs
+++ scrabble-score/src/Scrabble.hs
@@ -1,7 +1,24 @@
 module Scrabble (scoreLetter, scoreWord) where
 
+import Data.Char (toLower)
+import Data.Map (Map)
+import Data.Map.Strict as Map (findWithDefault, fromList)
+
 scoreLetter :: Char -> Integer
-scoreLetter letter = error "You need to implement this function."
+scoreLetter letter = Map.findWithDefault 0 letter preset
 
 scoreWord :: String -> Integer
-scoreWord word = error "You need to implement this function."
+scoreWord = sum . map scoreLetter
+
+preset :: Map Char Integer
+preset = Map.fromList $ do
+  (letters, score) <-
+    ("AEIOULNRST", 1) :
+    ("DG", 2) :
+    ("BCMP", 3) :
+    ("FHVWY", 4) :
+    ("K", 5) :
+    ("JX", 8) :
+    ("QZ", 10) : []
+  letter <- letters
+  [(letter, score), (toLower letter, score)]
