5984e34e008556f1f69865b16e0bbd4c1364fc8f
rapidfireworks <rapidfireworks@gmail.com>
2020-09-17 13:07:33 +0900

    Solve

--- triangle/src/Triangle.hs
+++ triangle/src/Triangle.hs
@@ -1,10 +1,19 @@
-module Triangle (TriangleType(..), triangleType) where
+module Triangle (TriangleType (..), triangleType) where
 
-data TriangleType = Equilateral
-                  | Isosceles
-                  | Scalene
-                  | Illegal
-                  deriving (Eq, Show)
+import Data.List as List (sort)
+
+data TriangleType
+  = Equilateral
+  | Isosceles
+  | Scalene
+  | Illegal
+  deriving (Eq, Show)
 
 triangleType :: (Num a, Ord a) => a -> a -> a -> TriangleType
-triangleType a b c = error "You need to implement this function."
+triangleType a b c = classify $ List.sort [a, b, c]
+  where
+    classify [x, y, z]
+      | x + y <= z = Illegal
+      | x == z = Equilateral
+      | x == y || y == z = Isosceles
+    classify _ = Scalene
