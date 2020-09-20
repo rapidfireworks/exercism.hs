30aeb91b3c7a27e17ce477200ff99187eca1c6da
rapidfireworks <rapidfireworks@gmail.com>
2020-09-20 18:26:09 +0900

    Refactor with tuple

--- complex-numbers/src/ComplexNumbers.hs
+++ complex-numbers/src/ComplexNumbers.hs
@@ -3,53 +3,39 @@ module ComplexNumbers (Complex, conjugate, abs, exp, real, imaginary, mul, add, 
 import Prelude hiding (abs, div, exp, (^))
 import qualified Prelude (exp, (^))
 
-data Complex a = Complex a a deriving (Eq, Show)
+type Complex a = (a, a)
 
 complex :: (a, a) -> Complex a
-complex (r, i) = Complex r i
+complex = id
 
 conjugate :: Num a => Complex a -> Complex a
-conjugate (Complex r i) = Complex r (- i)
+conjugate (a, b) = (a, - b)
 
 abs :: Floating a => Complex a -> a
-abs (Complex r i) = sqrt (r ^ 2 + i ^ 2)
+abs (a, b) = sqrt (a ^ 2 + b ^ 2)
 
 real :: Num a => Complex a -> a
-real (Complex r _) = r
+real = fst
 
 imaginary :: Num a => Complex a -> a
-imaginary (Complex _ i) = i
+imaginary = snd
 
 exp :: Floating a => Complex a -> Complex a
-exp (Complex r i) = Complex (e * c) (e * s)
+exp (r, i) = (e * cos i, e * sin i)
   where
     e = Prelude.exp r
-    c = cos i
-    s = sin i
 
 mul :: Num a => Complex a -> Complex a -> Complex a
-mul lhs rhs = Complex (dot conj rhs) (det conj rhs)
-  where
-    conj = conjugate lhs
+mul (a, b) (c, d) = (a * c - b * d, b * c + a * d)
 
 add :: Num a => Complex a -> Complex a -> Complex a
-add (Complex r1 i1) (Complex r2 i2) = Complex (r1 + r2) (i1 + i2)
+add (a, b) (c, d) = (a + c, b + d)
 
 sub :: Num a => Complex a -> Complex a -> Complex a
-sub (Complex r1 i1) (Complex r2 i2) = Complex (r1 - r2) (i1 - i2)
+sub (a, b) (c, d) = (a - c, b - d)
 
-div :: Floating a => Complex a -> Complex a -> Complex a
-div lhs rhs = Complex (r1 / r2) (i1 / r2)
-  where
-    conj = conjugate rhs
-    Complex r1 i1 = mul lhs conj
-    Complex r2 _ = mul rhs conj
-
-dot :: Num a => Complex a -> Complex a -> a
-dot (Complex a b) (Complex c d) = a * c + b * d
-
-det :: Num a => Complex a -> Complex a -> a
-det (Complex a b) (Complex c d) = a * d - b * c
+div :: Fractional a => Complex a -> Complex a -> Complex a
+div (a, b) (c, d) = ((a * c + b * d) / (c ^ 2 + d ^ 2), (b * c - a * d) / (c ^ 2 + d ^ 2))
 
 (^) :: (Num a) => a -> Integer -> a
 (^) = (Prelude.^)
