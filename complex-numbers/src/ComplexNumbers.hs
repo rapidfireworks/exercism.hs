64ecfe825fa2c78b6f3b9bac0bbf666959c304aa
rapidfireworks <rapidfireworks@gmail.com>
2020-09-20 18:16:50 +0900

    Solve

--- complex-numbers/src/ComplexNumbers.hs
+++ complex-numbers/src/ComplexNumbers.hs
@@ -1,49 +1,55 @@
-module ComplexNumbers
-(Complex,
- conjugate,
- abs,
- exp,
- real,
- imaginary,
- mul,
- add,
- sub,
- div,
- complex) where
+module ComplexNumbers (Complex, conjugate, abs, exp, real, imaginary, mul, add, sub, div, complex) where
 
-import Prelude hiding (div, abs, exp)
+import Prelude hiding (abs, div, exp, (^))
+import qualified Prelude (exp, (^))
 
--- Data definition -------------------------------------------------------------
-data Complex a = Dummy deriving(Eq, Show)
+data Complex a = Complex a a deriving (Eq, Show)
 
 complex :: (a, a) -> Complex a
-complex = error "You need to implement this function"
+complex (r, i) = Complex r i
 
--- unary operators -------------------------------------------------------------
 conjugate :: Num a => Complex a -> Complex a
-conjugate = error "You need to implement this function"
+conjugate (Complex r i) = Complex r (- i)
 
 abs :: Floating a => Complex a -> a
-abs = error "You need to implement this function"
+abs (Complex r i) = sqrt (r ^ 2 + i ^ 2)
 
 real :: Num a => Complex a -> a
-real = error "You need to implement this function"
+real (Complex r _) = r
 
 imaginary :: Num a => Complex a -> a
-imaginary = error "You need to implement this function"
+imaginary (Complex _ i) = i
 
 exp :: Floating a => Complex a -> Complex a
-exp = error "You need to implement this function"
+exp (Complex r i) = Complex (e * c) (e * s)
+  where
+    e = Prelude.exp r
+    c = cos i
+    s = sin i
 
--- binary operators ------------------------------------------------------------
 mul :: Num a => Complex a -> Complex a -> Complex a
-mul = error "You need to implement this function"
+mul lhs rhs = Complex (dot conj rhs) (det conj rhs)
+  where
+    conj = conjugate lhs
 
 add :: Num a => Complex a -> Complex a -> Complex a
-add = error "You need to implement this function"
+add (Complex r1 i1) (Complex r2 i2) = Complex (r1 + r2) (i1 + i2)
 
 sub :: Num a => Complex a -> Complex a -> Complex a
-sub = error "You need to implement this function"
+sub (Complex r1 i1) (Complex r2 i2) = Complex (r1 - r2) (i1 - i2)
 
-div :: Fractional a => Complex a -> Complex a -> Complex a
-div = error "You need to implement this function"
+div :: Floating a => Complex a -> Complex a -> Complex a
+div lhs rhs = Complex (r1 / r2) (i1 / r2)
+  where
+    conj = conjugate rhs
+    Complex r1 i1 = mul lhs conj
+    Complex r2 _ = mul rhs conj
+
+dot :: Num a => Complex a -> Complex a -> a
+dot (Complex a b) (Complex c d) = a * c + b * d
+
+det :: Num a => Complex a -> Complex a -> a
+det (Complex a b) (Complex c d) = a * d - b * c
+
+(^) :: (Num a) => a -> Integer -> a
+(^) = (Prelude.^)
