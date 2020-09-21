96ce0507e0629337d0f3e38f137b041b99b7d0b6
rapidfireworks <rapidfireworks@gmail.com>
2020-09-21 10:02:16 +0900

    Refactor with length

--- armstrong-numbers/src/ArmstrongNumbers.hs
+++ armstrong-numbers/src/ArmstrongNumbers.hs
@@ -4,16 +4,5 @@ armstrong :: Integral a => a -> Bool
 armstrong n | n < 0 = False
 armstrong n = armstrongSum == toInteger n
   where
-    armstrongSum = sum $ do
-      digit <- show $ toInteger n
-      return (read [digit] ^ digitCount n)
-
-digitCount :: (Integral a) => a -> a
-digitCount n | n < 0 = digitCount $ abs n
-digitCount 0 = 1
-digitCount n = 1 + truncLog n
-  where
-    truncLog = truncate . log10 . fromIntegral
-
-log10 :: Double -> Double
-log10 = logBase 10
+    digits = show $ toInteger n
+    armstrongSum = sum [read [digit] ^ length digits | digit <- digits]
