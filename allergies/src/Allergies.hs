ef0452f0e392132b44f0ffed574f645666f6f682
rapidfireworks <rapidfireworks@gmail.com>
2020-09-24 09:28:01 +0900

    Solve

--- allergies/src/Allergies.hs
+++ allergies/src/Allergies.hs
@@ -1,17 +1,30 @@
-module Allergies (Allergen(..), allergies, isAllergicTo) where
+module Allergies (Allergen (..), allergies, isAllergicTo) where
 
-data Allergen = Eggs
-              | Peanuts
-              | Shellfish
-              | Strawberries
-              | Tomatoes
-              | Chocolate
-              | Pollen
-              | Cats
-              deriving (Eq, Show)
+import Data.Bits ((.&.))
+
+data Allergen
+  = Eggs
+  | Peanuts
+  | Shellfish
+  | Strawberries
+  | Tomatoes
+  | Chocolate
+  | Pollen
+  | Cats
+  deriving (Eq, Show, Enum)
 
 allergies :: Int -> [Allergen]
-allergies score = error "You need to implement this function."
+allergies score = filter (flip isAllergicTo score) [Eggs ..]
 
 isAllergicTo :: Allergen -> Int -> Bool
-isAllergicTo allergen score = error "You need to implement this function."
+isAllergicTo allergen score = toScore allergen .&. score /= 0
+
+toScore :: Allergen -> Int
+toScore Eggs = 1
+toScore Peanuts = 2
+toScore Shellfish = 4
+toScore Strawberries = 8
+toScore Tomatoes = 16
+toScore Chocolate = 32
+toScore Pollen = 64
+toScore Cats = 128
