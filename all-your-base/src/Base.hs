aba0480fced7523beb2687cce2fd7a8f62e56caf
rapidfireworks <rapidfireworks@gmail.com>
2020-09-24 12:30:12 +0900

    Solve

--- all-your-base/src/Base.hs
+++ all-your-base/src/Base.hs
@@ -1,7 +1,30 @@
-module Base (Error(..), rebase) where
+module Base (Error (..), rebase) where
+
+import Control.Monad (foldM)
 
 data Error a = InvalidInputBase | InvalidOutputBase | InvalidDigit a
-    deriving (Show, Eq)
+  deriving (Show, Eq)
 
 rebase :: Integral a => a -> a -> [a] -> Either (Error a) [a]
-rebase inputBase outputBase inputDigits = error "You need to implement this function."
+rebase inputBase outputBase inputDigits =
+  rebaseFrom inputBase inputDigits
+    >>= rebaseTo outputBase
+
+rebaseFrom :: (Integral a) => a -> [a] -> Either (Error a) a
+rebaseFrom inputBase inputDigits
+  | inputBase < 2 = Left InvalidInputBase
+  | otherwise = foldM go 0 inputDigits
+  where
+    go result digit
+      | digit >= 0 && digit < inputBase = Right $ result * inputBase + digit
+      | otherwise = Left $ InvalidDigit digit
+
+rebaseTo :: Integral a => a -> a -> Either (Error a) [a]
+rebaseTo outputBase n
+  | outputBase < 2 = Left InvalidOutputBase
+  | otherwise = Right $ _rebaseTo outputBase n []
+  where
+    _rebaseTo _ 0 result = result
+    _rebaseTo base k result = _rebaseTo base q (r : result)
+      where
+        (q, r) = quotRem k base
