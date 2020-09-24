d4c81e1edc4d212baac30e44afe492de76edc446
rapidfireworks <rapidfireworks@gmail.com>
2020-09-24 09:46:54 +0900

    Refactor with testBit

--- allergies/src/Allergies.hs
+++ allergies/src/Allergies.hs
@@ -1,6 +1,6 @@
 module Allergies (Allergen (..), allergies, isAllergicTo) where
 
-import Data.Bits ((.&.))
+import Data.Bits (testBit)
 
 data Allergen
   = Eggs
@@ -17,14 +17,4 @@ allergies :: Int -> [Allergen]
 allergies score = filter (`isAllergicTo` score) [Eggs ..]
 
 isAllergicTo :: Allergen -> Int -> Bool
-isAllergicTo allergen score = toScore allergen .&. score /= 0
-
-toScore :: Allergen -> Int
-toScore Eggs = 1
-toScore Peanuts = 2
-toScore Shellfish = 4
-toScore Strawberries = 8
-toScore Tomatoes = 16
-toScore Chocolate = 32
-toScore Pollen = 64
-toScore Cats = 128
+isAllergicTo allergen score = testBit score $ fromEnum allergen
