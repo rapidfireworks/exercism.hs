4fdffd3fac2da516e24e8e375872a3f99181e4a0
rapidfireworks <rapidfireworks@gmail.com>
2020-10-25 16:32:15 +0900

    Solve

--- binary-search/src/BinarySearch.hs
+++ binary-search/src/BinarySearch.hs
@@ -1,6 +1,18 @@
 module BinarySearch (find) where
 
-import Data.Array
+import Data.Array (Array, bounds, (!))
 
 find :: Ord a => Array Int a -> a -> Maybe Int
-find array x = error "You need to implement this function."
+find arr x
+  | (startIndex > endIndex) = Nothing
+  | otherwise = go startIndex endIndex
+  where
+    (startIndex, endIndex) = bounds arr
+    go l u
+      | l >= u && x /= pivot = Nothing
+      | x == pivot = Just i
+      | x < pivot = go l i
+      | otherwise = go (i + 1) u
+      where
+        i = quot (l + u) 2
+        pivot = arr ! i
