04be13bac86fc140295cc50de2d4486cd9674551
rapidfireworks <rapidfireworks@gmail.com>
2020-10-26 13:40:58 +0900

    Solve

--- atbash-cipher/src/Atbash.hs
+++ atbash-cipher/src/Atbash.hs
@@ -1,7 +1,33 @@
 module Atbash (decode, encode) where
 
+import Data.Char (isAlpha, isAlphaNum, toLower)
+import Data.Function ((&))
+
 decode :: String -> String
-decode cipherText = error "You need to implement this function."
+decode cipherText =
+  sanitize cipherText
+    & map convert
 
 encode :: String -> String
-encode plainText = error "You need to implement this function."
+encode plainText =
+  sanitize plainText
+    & map convert
+    & space
+
+sanitize :: [Char] -> [Char]
+sanitize xs = [toLower x | x <- xs, isAlphaNum x]
+
+convert :: Char -> Char
+convert x
+  | isAlpha x = toEnum result
+  | otherwise = x
+  where
+    result = fromEnum 'z' - fromEnum x + fromEnum 'a'
+
+space :: [Char] -> [Char]
+space [] = []
+space (hd : xs) =
+  hd : do
+    (x, s) <- zip xs $ cycle [Nothing, Nothing, Nothing, Nothing, Just ' ']
+    Just y <- [s, Just x]
+    return y
