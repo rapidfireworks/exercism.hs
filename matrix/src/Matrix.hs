613e8840c75f961ea77c2e5530a49853bfce6235
rapidfireworks <rapidfireworks@gmail.com>
2020-10-28 11:34:52 +0900

    Solve

--- matrix/src/Matrix.hs
+++ matrix/src/Matrix.hs
@@ -1,47 +1,63 @@
 module Matrix
-    ( Matrix
-    , cols
-    , column
-    , flatten
-    , fromList
-    , fromString
-    , reshape
-    , row
-    , rows
-    , shape
-    , transpose
-    ) where
+  ( Matrix,
+    cols,
+    column,
+    flatten,
+    fromList,
+    fromString,
+    reshape,
+    row,
+    rows,
+    shape,
+    transpose,
+  )
+where
 
-import Data.Vector (Vector)
+import Data.Functor ((<&>))
+import qualified Data.Text as T
+import Data.Vector (Vector, (!))
+import qualified Data.Vector as V
+import Text.Read (readMaybe)
 
-data Matrix a = Dummy deriving (Eq, Show)
+data Matrix a = Matrix (Vector (Vector a)) deriving (Eq, Show)
 
 cols :: Matrix a -> Int
-cols matrix = error "You need to implement this function."
+cols (Matrix xss) = V.foldl' max 0 $ V.length <$> xss
 
 column :: Int -> Matrix a -> Vector a
-column x matrix = error "You need to implement this function."
+column x (Matrix xss) = xss <&> (! pred x)
 
 flatten :: Matrix a -> Vector a
-flatten matrix = error "You need to implement this function."
+flatten (Matrix xss) = V.concatMap id xss
 
 fromList :: [[a]] -> Matrix a
-fromList xss = error "You need to implement this function."
+fromList xss = Matrix $ V.fromList $ V.fromList <$> xss
 
 fromString :: Read a => String -> Matrix a
-fromString xs = error "You need to implement this function."
+fromString xs = fromList $ do
+  line <- T.lines $ T.pack xs
+  return $ do
+    word <- T.words line
+    Just n <- pure $ readMaybe $ T.unpack word
+    return n
 
 reshape :: (Int, Int) -> Matrix a -> Matrix a
-reshape dimensions matrix = error "You need to implement this function."
+reshape (r, c) matrix = Matrix $ do
+  startIndex <- V.enumFromStepN 0 c r
+  return $ V.slice startIndex c xs
+  where
+    xs = flatten matrix
 
 row :: Int -> Matrix a -> Vector a
-row x matrix = error "You need to implement this function."
+row x (Matrix xss) = xss ! pred x
 
 rows :: Matrix a -> Int
-rows matrix = error "You need to implement this function."
+rows (Matrix xss) = V.length xss
 
 shape :: Matrix a -> (Int, Int)
-shape matrix = error "You need to implement this function."
+shape matrix = (rows matrix, cols matrix)
 
 transpose :: Matrix a -> Matrix a
-transpose matrix = error "You need to implement this function."
+transpose matrix = Matrix $ do
+  x <- V.enumFromN 1 $ cols matrix
+  return $ column x matrix
