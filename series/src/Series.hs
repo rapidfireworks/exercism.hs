c4abde8a8e40e9bd95ccb132d15ded63dfd9c3bc
rapidfireworks <rapidfireworks@gmail.com>
2020-11-02 13:20:08 +0900

    Refactor with tail

--- series/src/Series.hs
+++ series/src/Series.hs
@@ -1,19 +1,13 @@
 module Series (slices) where
 
-import qualified Data.List.NonEmpty as NE (toList, unfoldr)
+import Data.List (tails)
 
 slices :: Int -> String -> [[Int]]
 slices n xs = do
   Just slice <- takeWhile (not . null) result
   return slice
   where
-    result = takeExact n <$> heads [read [x] | x <- xs]
-
-heads :: [a] -> [[a]]
-heads = NE.toList . NE.unfoldr go
-  where
-    go [] = ([], Nothing)
-    go xs@(_ : tl) = (xs, Just tl)
+    result = takeExact n <$> tails [read [x] | x <- xs]
 
 takeExact :: Int -> [a] -> Maybe [a]
 takeExact 0 _ = Just []
