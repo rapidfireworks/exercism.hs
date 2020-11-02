b2334aebe221e137647a8e0659f12a495c0315c0
rapidfireworks <rapidfireworks@gmail.com>
2020-11-02 13:12:02 +0900

    Solve

--- series/src/Series.hs
+++ series/src/Series.hs
@@ -1,4 +1,21 @@
 module Series (slices) where
 
+import qualified Data.List.NonEmpty as NE (toList, unfoldr)
+
 slices :: Int -> String -> [[Int]]
-slices n xs = error "You need to implement this function."
+slices n xs = do
+  Just slice <- takeWhile (not . null) result
+  return slice
+  where
+    result = takeExact n <$> heads [read [x] | x <- xs]
+
+heads :: [a] -> [[a]]
+heads = NE.toList . NE.unfoldr go
+  where
+    go [] = ([], Nothing)
+    go xs@(_ : tl) = (xs, Just tl)
+
+takeExact :: Int -> [a] -> Maybe [a]
+takeExact 0 _ = Just []
+takeExact _ [] = Nothing
+takeExact n (x : xs) = (x :) <$> takeExact (pred n) xs
