71d6fbd52913ab1162e0d4ee810f8f9f1103ddf5
rapidfireworks <rapidfireworks@gmail.com>
2020-11-03 10:07:58 +0900

    Refactor with zipWith

--- series/src/Series.hs
+++ series/src/Series.hs
@@ -3,13 +3,6 @@ module Series (slices) where
 import Data.List (tails)
 
 slices :: Int -> String -> [[Int]]
-slices n xs = do
-  Just slice <- takeWhile (not . null) result
-  return slice
+slices n xs = take n <$> zipWith const xss (drop n xss)
   where
-    result = takeExact n <$> tails [read [x] | x <- xs]
-
-takeExact :: Int -> [a] -> Maybe [a]
-takeExact 0 _ = Just []
-takeExact _ [] = Nothing
-takeExact n (x : xs) = (x :) <$> takeExact (pred n) xs
+    xss = tails $ read <$> pure <$> xs
