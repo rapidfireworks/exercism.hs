2a65e0af941f58c04f05efa206e0afb16de01230
rapidfireworks <rapidfireworks@gmail.com>
2020-11-03 19:16:22 +0900

    Solve

--- binary-search-tree/src/BST.hs
+++ binary-search-tree/src/BST.hs
@@ -1,37 +1,47 @@
 module BST
-    ( BST
-    , bstLeft
-    , bstRight
-    , bstValue
-    , empty
-    , fromList
-    , insert
-    , singleton
-    , toList
-    ) where
+  ( BST,
+    bstLeft,
+    bstRight,
+    bstValue,
+    empty,
+    fromList,
+    insert,
+    singleton,
+    toList,
+  )
+where
 
-data BST a = Dummy deriving (Eq, Show)
+import Data.Foldable (foldl')
+
+data BST a = BST (Maybe (a, BST a, BST a)) deriving (Eq, Show)
 
 bstLeft :: BST a -> Maybe (BST a)
-bstLeft tree = error "You need to implement this function."
+bstLeft (BST (Just (_, x, _))) = Just x
+bstLeft _ = Nothing
 
 bstRight :: BST a -> Maybe (BST a)
-bstRight tree = error "You need to implement this function."
+bstRight (BST (Just (_, _, x))) = Just x
+bstRight _ = Nothing
 
 bstValue :: BST a -> Maybe a
-bstValue tree = error "You need to implement this function."
+bstValue (BST (Just (x, _, _))) = Just x
+bstValue _ = Nothing
 
 empty :: BST a
-empty = error "You need to implement this function."
+empty = BST Nothing
 
 fromList :: Ord a => [a] -> BST a
-fromList xs = error "You need to implement this function."
+fromList xs = foldl' (flip insert) empty xs
 
 insert :: Ord a => a -> BST a -> BST a
-insert x tree = error "You need to implement this function."
+insert x (BST (Just (y, lhs, rhs)))
+  | x > y = BST (Just (y, lhs, insert x rhs))
+  | otherwise = BST (Just (y, insert x lhs, rhs))
+insert x _ = singleton x
 
 singleton :: a -> BST a
-singleton x = error "You need to implement this function."
+singleton x = BST (Just (x, empty, empty))
 
 toList :: BST a -> [a]
-toList tree = error "You need to implement this function."
+toList (BST (Just (x, lhs, rhs))) = toList lhs ++ x : toList rhs
+toList _ = []
