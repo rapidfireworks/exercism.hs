b2efc7a41e3dd813e134dc4c481fa4f93359059d
rapidfireworks <rapidfireworks@gmail.com>
2020-11-03 18:03:23 +0900

    Solve

--- matching-brackets/src/Brackets.hs
+++ matching-brackets/src/Brackets.hs
@@ -1,4 +1,20 @@
 module Brackets (arePaired) where
 
+import Data.Foldable (foldl')
+
 arePaired :: String -> Bool
-arePaired xs = error "You need to implement this function."
+arePaired xs = null $ foldl' go [] [x | x <- xs, isBracket x]
+  where
+    go ('[' : ys) ']' = ys
+    go ('{' : ys) '}' = ys
+    go ('(' : ys) ')' = ys
+    go ys y = y : ys
+
+isBracket :: Char -> Bool
+isBracket '[' = True
+isBracket ']' = True
+isBracket '{' = True
+isBracket '}' = True
+isBracket '(' = True
+isBracket ')' = True
+isBracket _ = False
