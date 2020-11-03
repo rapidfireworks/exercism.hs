01994392d11ffcf0dd4a82cb8e4d698304d8819f
rapidfireworks <rapidfireworks@gmail.com>
2020-11-03 19:25:22 +0900

    Refactor removing tuple

--- binary-search-tree/src/BST.hs
+++ binary-search-tree/src/BST.hs
@@ -13,35 +13,35 @@ where
 
 import Data.Foldable (foldl')
 
-data BST a = Empty | BST (a, BST a, BST a) deriving (Eq, Show)
+data BST a = Empty | BST a (BST a) (BST a) deriving (Eq, Show)
 
 bstLeft :: BST a -> Maybe (BST a)
-bstLeft (BST (_, x, _)) = Just x
+bstLeft (BST _ x _) = Just x
 bstLeft _ = Nothing
 
 bstRight :: BST a -> Maybe (BST a)
-bstRight (BST (_, _, x)) = Just x
+bstRight (BST _ _ x) = Just x
 bstRight _ = Nothing
 
 bstValue :: BST a -> Maybe a
-bstValue (BST (x, _, _)) = Just x
+bstValue (BST x _ _) = Just x
 bstValue _ = Nothing
 
 empty :: BST a
 empty = Empty
 
 fromList :: Ord a => [a] -> BST a
-fromList xs = foldl' (flip insert) empty xs
+fromList xs = foldl' (flip insert) Empty xs
 
 insert :: Ord a => a -> BST a -> BST a
-insert x (BST (y, lhs, rhs))
-  | x > y = BST (y, lhs, insert x rhs)
-  | otherwise = BST (y, insert x lhs, rhs)
+insert x (BST y lhs rhs)
+  | x > y = BST y lhs (insert x rhs)
+  | otherwise = BST y (insert x lhs) rhs
 insert x _ = singleton x
 
 singleton :: a -> BST a
-singleton x = BST (x, empty, empty)
+singleton x = BST x Empty Empty
 
 toList :: BST a -> [a]
-toList (BST (x, lhs, rhs)) = toList lhs ++ x : toList rhs
+toList (BST x lhs rhs) = toList lhs ++ x : toList rhs
 toList _ = []
