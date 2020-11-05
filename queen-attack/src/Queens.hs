4a84210acaeabfcc38f06d55f075360cf5f59cec
rapidfireworks <rapidfireworks@gmail.com>
2020-11-05 13:27:33 +0900

    Solve

--- queen-attack/src/Queens.hs
+++ queen-attack/src/Queens.hs
@@ -1,7 +1,19 @@
 module Queens (boardString, canAttack) where
 
 boardString :: Maybe (Int, Int) -> Maybe (Int, Int) -> String
-boardString white black = error "You need to implement this function."
+boardString white black = unlines $ row <$> indices
+  where
+    indices = [0 .. 7]
+    row x = unwords $ square <$> indices
+      where
+        square y
+          | white == pure (x, y) = "W"
+          | black == pure (x, y) = "B"
+          | otherwise = "_"
 
 canAttack :: (Int, Int) -> (Int, Int) -> Bool
-canAttack queenA queenB = error "You need to implement this function."
+canAttack (x1, y1) (x2, y2) =
+  x1 == x2
+    || y1 == y2
+    || x1 - y1 == x2 - y2
+    || x1 + y1 == x2 + y2
