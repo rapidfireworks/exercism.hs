a3c73cea6357e19c9a2d2b159f4fb421c2bac1b4
rapidfireworks <rapidfireworks@gmail.com>
2020-11-18 10:08:34 +0900

    Solve

--- ocr-numbers/src/OCR.hs
+++ ocr-numbers/src/OCR.hs
@@ -1,4 +1,22 @@
 module OCR (convert) where
 
+import Data.List (intercalate, transpose)
+import Data.List.Split (chunksOf)
+
 convert :: String -> String
-convert xs = error "You need to implement this function."
+convert = intercalate "," . map parseLine . chunksOf 4 . lines
+  where
+    parseLine = map parse . transpose . map (chunksOf 3)
+
+parse :: [String] -> Char
+parse [" _ ", "| |", "|_|", "   "] = '0'
+parse ["   ", "  |", "  |", "   "] = '1'
+parse [" _ ", " _|", "|_ ", "   "] = '2'
+parse [" _ ", " _|", " _|", "   "] = '3'
+parse ["   ", "|_|", "  |", "   "] = '4'
+parse [" _ ", "|_ ", " _|", "   "] = '5'
+parse [" _ ", "|_ ", "|_|", "   "] = '6'
+parse [" _ ", "  |", "  |", "   "] = '7'
+parse [" _ ", "|_|", "|_|", "   "] = '8'
+parse [" _ ", "|_|", " _|", "   "] = '9'
+parse _ = '?'
