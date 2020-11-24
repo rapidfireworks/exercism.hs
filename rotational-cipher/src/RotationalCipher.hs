7525c56b14de860d0f5fd627cb955b39a5c6bc47
rapidfireworks <rapidfireworks@gmail.com>
2020-11-24 12:35:17 +0900

    Solve

--- rotational-cipher/src/RotationalCipher.hs
+++ rotational-cipher/src/RotationalCipher.hs
@@ -1,4 +1,14 @@
 module RotationalCipher (rotate) where
 
+import Data.Char
+
 rotate :: Int -> String -> String
-rotate = error "You need to implement this function."
+rotate offset = map (chr . go)
+  where
+    go x
+      | isUpper x = upA + mod (ord x - upA + offset) period
+      | isLower x = loA + mod (ord x - loA + offset) period
+      | otherwise = ord x
+    upA = ord 'A'
+    loA = ord 'a'
+    period = ord 'z' - loA + 1
